694df33c4c294ca2bc1c25c08d86c2224a6a80df
Damyres <damyresmaciell@gmail.com>
2024-08-14 19:43:37 -0300

    Criar função para inserir vídeo ultilizando a api

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -4,7 +4,29 @@ async function listaVideos() {
     return conexaoConvertida
 }
 
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch("http://localhost:3000/videos", {
+        //método a ser realizado
+        method: "POST",
+
+        //tipo de requisição
+        headers: {
+            "Content-type": "application/json"
+        },
+
+        //corpo da requisição
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil vizualizações`,
+            url: url,
+            imagem: imagem
+        })
+    })
+     conexaoConvertida = await conexao.json()
+     return conexaoConvertida
+}
+
 export const conectaApi = {
-    listaVideos
+    listaVideos, criaVideo
 }
 
