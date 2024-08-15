625e758b8e06ef73a16d26bda1df6023255d2f06
Damyres <damyresmaciell@gmail.com>
2024-08-15 18:46:57 -0300

    Tradando erro no carregamento da lista de videos

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -6,7 +6,7 @@ export default function constroiCard(titulo, descricao, url, imagem) {
     const video = document.createElement('li');
     video.className = "videos__item";
     video.innerHTML =
-            `<iframe width="100%" height="72%" src="${url}"
+        `<iframe width="100%" height="72%" src="${url}"
                 title="${titulo}" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen></iframe>
@@ -19,10 +19,14 @@ export default function constroiCard(titulo, descricao, url, imagem) {
 }
 
 async function listaVideos() {
-    const listaApi = await conectaApi.listaVideos();
-    listaApi.forEach(elemento => 
-        lista.appendChild(
-            constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+    try {
+        const listaApi = await conectaApi.listaVideos();
+        listaApi.forEach(elemento =>
+            lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+    } catch {
+        lista.innerHTML= `<h2 class="message__titulo">Não foi possível carregar a lista de vídeos</h2>`
+    }
+
 }
 
 listaVideos();
